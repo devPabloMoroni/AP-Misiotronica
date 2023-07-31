0b384663aef29c541be4ec5b2195414f1eebda04
Pablo Moroni <pmoroni@untdf.edu.ar>
2023-07-31 16:27:29 -0300

    Modificacion en imagenes cards

--- src/components/singleComponents/ProductCard/ProductCard.jsx
+++ src/components/singleComponents/ProductCard/ProductCard.jsx
@@ -1,19 +1,22 @@
 import './Card.css';
 
 //TODO - Optimizar el manejo de imagenes agregando toda la carpeta o buscando otra alternativa.
-const images = {
+/* const images = {
   image1: require('../../../assets/img/products/Samsung-Galaxy S21-Celulares.jpg'),
   image2: require('../../../assets/img/products/Dell-Inspiron 15-Computadoras.jpg'),
   image3: require('../../../assets/img/products/Sony-Bravia 4K-Media.jpg'),
-};
+}; */
 
+/* const images = require.context('../../../assets/img/products', true);
+const imageList = images.keys().map(image => images(image));
+console.log(imageList); */
 
 const ProductCard = ({ producto }) => {
   return (
       <>
         <div class="col">
           <div class="card">
-            <img src={images.image1} class="card-img-top" alt="..." />
+            <img src={require("../../../assets/img/products/" + producto.imagen)} class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title"> {producto.nombre}</h5>
               <p class="card-text">Marca: {producto.marca}</p>
