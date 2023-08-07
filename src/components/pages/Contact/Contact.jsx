b64bed8ba55b656a74f504913f66b40de44a4f1f
Pablo Moroni <pmoroni@untdf.edu.ar>
2023-08-07 15:19:22 -0300

    Cambio nombre de class a className

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -18,15 +18,15 @@ const chat = <svg width="2em" height="2em" fill="currentColor" className="bi p-2
 
 const Contact = () => {
   return(
-    <div class="container">
+    <div className="container">
       <Aside />
       <div className="px-4 py-3 my-3 text-center">
         <h2 className="display-6 fw-bold">CONTACTO</h2>
       </div>
 
-      <div class="row align-items-md-stretch">
-        <div class="col-md-6 col-lg-4 mb-3">
-          <div class="h-100 p-5 bg-body-tertiary border rounded-3  text-center">
+      <div className="row align-items-md-stretch">
+        <div className="col-md-6 col-lg-4 mb-3">
+          <div className="h-100 p-5 bg-body-tertiary border rounded-3  text-center">
             <div className="feature-icon d-inline-flex align-items-center 
                 justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
                   {telefono}
@@ -36,8 +36,8 @@ const Contact = () => {
               <p>+549 3751 224488</p>
           </div>
         </div>
-        <div class="col-md-6 col-lg-4 mb-3">
-          <div class="h-100 p-5 bg-body-tertiary border rounded-3  text-center">
+        <div className="col-md-6 col-lg-4 mb-3">
+          <div className="h-100 p-5 bg-body-tertiary border rounded-3  text-center">
             <div className="feature-icon d-inline-flex align-items-center 
                 justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
                   {email}
@@ -51,8 +51,8 @@ const Contact = () => {
               </a>
             </div>
         </div>
-        <div class="col-md-12 col-lg-4 mb-3">
-          <div class="h-100 p-5 bg-body-tertiary border rounded-3  text-center">
+        <div className="col-md-12 col-lg-4 mb-3">
+          <div className="h-100 p-5 bg-body-tertiary border rounded-3  text-center">
             <div className="feature-icon d-inline-flex align-items-center 
                 justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
                   {chat}
